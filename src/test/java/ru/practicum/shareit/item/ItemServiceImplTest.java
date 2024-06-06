d5a3e5760f6e37656418cd2de46829c986394084
Di-Mark <dima.markin228@yandex.ru>
2024-06-06 19:50:05 +0300

    fix: test

--- src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.StatusBooking;
-import ru.practicum.shareit.item.comment.dao.CommentRepository;
 import ru.practicum.shareit.item.comment.dto.CommentDto;
 import ru.practicum.shareit.item.comment.model.Comment;
 import ru.practicum.shareit.item.dto.ItemDto;
